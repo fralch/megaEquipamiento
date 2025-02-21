ef0e609b676955f32786b8e2a524ef8808993c33
Fralch <ingfralch@gmail.com>
2025-02-21 10:10:28 -0500

    arreglando la configuracion de vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
-        host: "0.0.0.0", // Escucha en todas las IPs
-        port: 5173, // Puerto de Vite
-        strictPort: true, // Usa siempre este puerto
-      },
+        host: '127.0.0.1', // Cambia a 127.0.0.1 o localhost
+        port: 5173,
+        strictPort: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
